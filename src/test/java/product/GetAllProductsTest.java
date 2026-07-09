83c27c88c676a8001ed567522f61952eec721fe2
danielavescia <danivescia@gmail.com>
2026-07-09 12:12:56 -0300

    feat: add tests for GetAllProducts API and JSON schema validation

--- src/test/java/product/GetAllProductsTest.java
+++ src/test/java/product/GetAllProductsTest.java
@@ -16,6 +16,12 @@ import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.restassured.response.Response;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 @Feature("Produtos")
 @Story("Listar todos os Produtos do catálago")
@@ -38,5 +44,25 @@ public class GetAllProductsTest extends BaseTest{
     @Severity(SeverityLevel.BLOCKER)
     public void shouldReturnStatusCode200(){
        assertEquals(response.getStatusCode(),200, "Erro: Status code retornado foi: " + response.getStatusCode() + " e o esperado era 200.");
+    }
+
+    @Test(description = "GET products deve retornar Content-Type application/json")
+    @Severity(SeverityLevel.MINOR)
+    public void shouldReturnJsonContentType(){
+        assertTrue(response.getContentType().contains("application/json"),
+         "Erro: Content-Type retornado: " + response.getContentType() + "e o esperado era application/json");
+    }
+
+    @Test(description = "GET /products deve retornar um array com 20 produtos")
+    @Severity(SeverityLevel.CRITICAL)
+    public void shouldReturnExactlyTwentyProducts(){
+        assertNotNull(products, "A lista de produtos é nula");
+        assertEquals(products.size(), 20, "Quantidade de produtos retornada é de: " + products.size() + " e o esperado é 20");
+    }
+
+    @Test(description = "GET /products deve retornar um array de produtos com schema válido")
+    @Severity(SeverityLevel.CRITICAL)
+    public void shouldReturnEachProductWithValidSchema(){
+        assertThat(response.asString(), matchesJsonSchemaInClasspath("schemas/products-schema.json"));
     }
 }
