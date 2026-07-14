00b871a3ff27f389a51ff67f6ea72cde800d22a3
danielavescia <danivescia@gmail.com>
2026-07-14 15:17:13 -0300

    refactor: simplify Product class using Lombok annotations

--- src/main/java/com/db/model/Product.java
+++ src/main/java/com/db/model/Product.java
@@ -3,8 +3,18 @@ package com.db.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class Product {
 
     private Integer id;
@@ -20,81 +30,4 @@ public class Product {
     private String image;
 
     private Rating rating;
-
-    public Product(){
-
-    }
-
-    public Product(String title, Float price, String description, String category, String image, Rating rating) {
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.category = category;
-        this.image = image;
-        this.rating = rating;
-    }
-
-     public Product(String title, Float price, String description, String category, String image) {
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.category = category;
-        this.image = image;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Float getPrice(){
-        return price;
-    }
-
-    public void setPrice(Float price){
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Rating getRating() {
-        return rating;
-    }
-
-    public void setRating(Rating rating) {
-        this.rating = rating;
-    }
 }
