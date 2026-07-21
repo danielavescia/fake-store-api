23e1d31710c3e7dbf061b92b0326d4e9b9f5d085
danielavescia <danivescia@gmail.com>
2026-07-21 09:54:29 -0300

    feat: add User model and LoginTest for user authentication

---
+++ src/main/java/com/db/model/User.java
@@ -0,0 +1,19 @@
+package com.db.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter @Setter
+public class User {
+
+    private String username;
+
+    private String password;
+    
+}
---
+++ src/test/java/auth/LoginTest.java
@@ -0,0 +1,52 @@
+package auth;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.db.client.auth.AuthClient;    
+import com.db.model.User;
+
+import assertions.ApiAssertions;
+import base.BaseTest;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.qameta.allure.testng.Tag;
+import io.restassured.response.Response;
+
+/* Dados do usuário estão hardcode porque está presente na documentação da API */
+
+@Feature("Usuários")
+@Story("Login de Usuário")
+public class LoginTest extends BaseTest{
+
+    private static final AuthClient authClient = new AuthClient();
+
+    private Response response;
+
+    private User user = new User("johnd", "m38rmF$");
+
+    @BeforeClass
+    public void createProduct(){
+        response = authClient.login(user);
+    }
+
+    @Test(description = "Deve autenticar com sucesso e retornar status 201")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tag("smoke")
+    void shouldAuthenticateSuccessfullyAndReturnCreated(){
+        ApiAssertions.assertStatusCode(response, 201);
+    }
+
+    @Test(description = "Deve autenticar com sucesso e retornar JWT")
+    @Severity(SeverityLevel.CRITICAL)
+    @Tag("smoke")
+    void shouldAuthenticateSuccessfullyAndReturnJwt(){
+        assertThat(response.jsonPath().getString("token"), is(notNullValue()));
+    }
+}
